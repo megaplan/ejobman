2332b02e7eeec3a5cb0ed8fa9a26fc237af3100f
Arkady Drovosekov <arkdro@gmail.com>
2011-07-13 19:14:36 +0400

    ejobman_child_sup and ejobman_child modules with empty content added

---
+++ src/ejobman_child_sup.erl
@@ -0,0 +1,16 @@
+-module(ejobman_child_sup).
+-behaviour(supervisor).
+-export([start_link/0, init/1]).
+-define(RESTARTS, 25).
+-define(SECONDS, 5).
+
+start_link() ->
+    supervisor:start_link({local, ejobman_supervisor}, ejobman_sup, []).
+
+init(_Args) ->
+    Worker = {
+        ejobman_child, {ejobman_child, start_link, []},
+        permanent, brutal_kill, worker, [ejobman_child]
+        },
+    {ok, {{simple_one_for_one, ?RESTARTS, ?SECONDS},
+        [Worker]}}.
